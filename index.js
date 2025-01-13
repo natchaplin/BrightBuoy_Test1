62bf27a6c44dbdf725d91cc918c2afaf758ca3da
Nat Chaplin <nataliefchaplin@gmail.com>
2025-01-12 20:01:52 -0500

    git commit -m "attempts at updating nav. bar"

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // let div1 = document.getElementById("div1");
 // div1.style.fontSize = "4em";
+// The following code does not do anything at this moment. It is meant as a way to create a differemt type of navigation bar, however will not work.
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+//   }
+//   function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+//   }
